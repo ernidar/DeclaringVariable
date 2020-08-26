0cd8210fbd75a475872adabbcdb9a5f5d1045768
ernidar <ernidar20gmail.com>
2020-08-26 16:52:27 -0400

    Declaring Variables

---
+++ src/declaringVariable.java
@@ -0,0 +1,36 @@
+public class declaringVariable {
+    public static void main(String[] args) {
+        String sayHi= "Hey Mom and Dad!\nHere are some interesting details about the first country I'm visiting.";
+        String country = "Kenya";
+        double population = 49.7;
+        String capital = "Nairobi";
+        double currency = 103.84;
+        String response="I'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.";
+
+        System.out.println(sayHi);
+        System.out.println("Name : " + country);
+        System.out.println("Population Size : " + population + " milions");
+        System.out.println("Capital : " + capital );
+        System.out.println("Currency : One U.S Dollar equals $" + currency + "Kenyan Shillings!!");
+        System.out.println(response);
+    }
+
+
+
+
+
+//    Hey Mom and Dad!
+//
+//    Here are some interesting details about the first country I'm visiting.
+//
+//    Name: Kenya
+//
+//    Population Size: 49.7 million
+//
+//    Capital: Nairobi
+//
+//    Currency: One U.S Dollar equals $103.84 Kenyan Shillings!!
+//
+//    I'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later.
+
+}
